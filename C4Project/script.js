40a89dbb9318a46a1a50f7c2c01551bc410d9ca8
Eleanor Fan <ellafan227@gmail.com>
2021-12-09 19:30:49 -0500

    Completed static calendar and added styles

--- C4Project/script.js
+++ C4Project/script.js
@@ -14,11 +14,12 @@ function createCalendar() {
   const element = document.getElementById("calendar");
   element.appendChild(listItem);
   for (var i = 0; i < weekList.length; i++) {
-    const listItem = document.createElement("td");
+    const listItem = document.createElement("th");
     const node = document.createTextNode(weekList[i]);
     listItem.appendChild(node);
     const element = document.getElementsByTagName("TR")[0];
     element.appendChild(listItem);
+    element.getElementsByTagName("th")[i].setAttribute("class", "weekday")
   }
   //creates blank rows at beginning
   if (firstDay != 0) {
@@ -30,23 +31,35 @@ function createCalendar() {
     const listItem = document.createElement("td");
     const element = document.getElementsByTagName("TR")[1];
     element.appendChild(listItem);
+    element.getElementsByTagName("td")[i-1].setAttribute("class", "blank");
   }
   //creates days
   for (var i = 0; i < daysInMonth; i++) {
     let day = new Date(year, month, i);
     let weekday = day.getDay();
     let tableRow = (i + firstDay - weekday - 1)/7 + 1;
-    console.log(tableRow);
+    //creates row if sunday
     if (day.getDay() == 0) {
       const listItem = document.createElement("tr");
       const element = document.getElementById("calendar");
       element.appendChild(listItem);
     }
-    const listItem = document.createElement("td");
-    const node = document.createTextNode(i + 1);
-    listItem.appendChild(node);
+    //creates day
+    const container = document.createElement("td");
+    const dayDiv = document.createElement("div");
+    const text = document.createTextNode(i + 1);
     const element = document.getElementsByTagName("TR")[tableRow];
+    dayDiv.appendChild(text);
+    container.appendChild(dayDiv);
+    element.appendChild(container);
+  }
+  //creates blank days
+  let lastDay = new Date(year, month, daysInMonth).getDay();
+  for (var i = 7; i > lastDay; i--) {
+    const listItem = document.createElement("td");
+    const element = document.getElementsByTagName("TR")[5];
     element.appendChild(listItem);
+    element.lastChild.setAttribute("class", "blank");
   }
   //note to self: 8-4
 }
