b19dbef72bc31f220ce9670c554ca093422167cc
Eleanor Fan <ellafan227@gmail.com>
2022-02-07 09:59:24 -0500

    Altered D1L1 and added D1L3

---
+++ Unit6-Java/D1L3/AsciiArt.java
@@ -0,0 +1,5 @@
+class AsciiArt {
+  public static void main(String[] args) {
+    System.out.println("\n    /----|---| \n   / 0   |   | \no--      |   | \n\\________|   | \n          \\__/");
+  }
+}
