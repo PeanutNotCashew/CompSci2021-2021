c0455ec8a23fa1b02927cba6c169648f138c7b01
Eleanor Fan <ellafan227@gmail.com>
2022-02-09 12:09:37 -0500

    Completed D1L5 work

---
+++ Unit6-Java/D1L5/GuessingGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class GuessingGame{
+  public static void takeAGuess(String ans, int tries) {
+    if (tries < 3) {
+      Scanner userInput = new Scanner(System.in);
+      System.out.println("Guess #" + ++tries + "/3:");
+      String guess = userInput.nextLine();
+
+      if (guess.equals(ans)) {
+        System.out.println("Your guess was correct!");
+      } else {
+        System.out.println("Your guess was incorrect.");
+        takeAGuess(ans, tries);
+      }
+    } else {
+      System.out.print("The answer was " + ans + ".");
+    }
+  }
+
+
+  public static void main(String[] args) {
+    int randNum = (int) (Math.random() * 10 + 1); //generates rand # from 1-10
+    String answer = Integer.toString(randNum);
+    int numOfTries = 0;
+
+    takeAGuess(answer, 0);
+
+  }
+}
