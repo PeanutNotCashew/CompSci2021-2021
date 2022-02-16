f81e2697eb9273193b9418b63876b07254cd06ca
Eleanor Fan <ellafan227@gmail.com>
2022-02-16 12:31:14 -0500

    D1L7: minor corrections

--- Unit6-Java/D1L9/sort.java
+++ Unit6-Java/D1L9/sort.java
@@ -1,6 +1,6 @@
 public class sort {
 
-  public static void bubbleSort(String[] array)  {
+  public static void bubbleSort(String[] array) {
     String temp;
     for (int i=0; i<array.length; i++){
       for(int j=1; j<array.length-i; j++) {
@@ -14,21 +14,24 @@ public class sort {
     }
   }
 
-  public static void main(String[] args) {
-    //declare array
-    String[] classArray = {"Arshiah", "Ahad", "Omar", "Jesse", "Anamaria", "Steve", "Otto", "Aunirbhan", "Prisha", "Bhavi", "Malak", "Eleanor", "Jeremiah", "Ace", "Petra", "Matthew", "Camilla", "Drishya", "Kate", "Leo", "Sophia", "Stephen", "Ioannis", "Angelina", "Mennaallah", "Fatima", "Tenzin", "Nabeel", "Valentina", "Martyna", "Abhijit", "Patrick", "Haocheng"};
-
-    //sorts
-    bubbleSort(classArray);
-
-    //prints with additional formatting
-    for (int i=0; i < classArray.length;) {
-      System.out.print(classArray[i]);
-      if (++i != classArray.length) {
+  public static void printArray(String[] array) {
+    for (int i=0; i < array.length;) {
+      System.out.print(array[i]);
+      if (++i != array.length) {
         System.out.print(", ");
       } else {
         System.out.print("\n");
       }
     }
+  }
+
+
+  public static void main(String[] args) {
+    //declare array
+    String[] classArray = {"Arshiah", "Ahad", "Omar", "Jesse", "Anamaria", "Steve", "Otto", "Aunirbhan", "Prisha", "Bhavi", "Malak", "Eleanor", "Jeremiah", "Ace", "Petra", "Matthew", "Camilla", "Drishya", "Kate", "Leo", "Sophia", "Stephen", "Ioannis", "Angelina", "Mennaallah", "Fatima", "Tenzin", "Nabeel", "Valentina", "Martyna", "Abhijit", "Patrick", "Haocheng"};
+
+    printArray(classArray);
+    bubbleSort(classArray);
+    printArray(classArray);
   }
 }
