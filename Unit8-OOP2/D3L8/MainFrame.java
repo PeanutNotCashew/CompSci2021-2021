ac4e3526f751b361e0dfa5a1143976ac02beef9d
Eleanor Fan <ellafan227@gmail.com>
2022-05-27 12:54:14 -0400

    Add minor edits and executable .jar file

--- Unit8-OOP2/D3L8/MainFrame.java
+++ Unit8-OOP2/D3L8/MainFrame.java
@@ -17,10 +17,9 @@ public class MainFrame extends JFrame implements ActionListener {
 	private JTextField lNameField;
 	private JButton okButton;
 	private JButton clrButton;
+	private JLabel output;
 
-	/**
-	 * Launch the application.
-	 */
+	// Launch application
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -34,9 +33,7 @@ public class MainFrame extends JFrame implements ActionListener {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	// Create frame
 	public MainFrame() {
 		setTitle("MainFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,16 +42,6 @@ public class MainFrame extends JFrame implements ActionListener {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		okButton = new JButton("OK");
-		okButton.setBounds(34, 200, 117, 29);
-		okButton.addActionListener(this);
-		contentPane.add(okButton);
-
-		clrButton = new JButton("Clear");
-		clrButton.setBounds(284, 200, 117, 29);
-		clrButton.addActionListener(this);
-		contentPane.add(clrButton);
 
 		JLabel fNameLabel = new JLabel("First Name");
 		fNameLabel.setBounds(34, 20, 117, 16);
@@ -73,15 +60,35 @@ public class MainFrame extends JFrame implements ActionListener {
 		lNameField.setBounds(44, 116, 243, 26);
 		contentPane.add(lNameField);
 		lNameField.setColumns(10);
+
+		output = new JLabel("");
+		output.setBounds(34, 150, 200, 26);
+		contentPane.add(output);
+
+		okButton = new JButton("OK");
+		okButton.setBounds(34, 200, 117, 29);
+		okButton.addActionListener(this);
+		contentPane.add(okButton);
+
+		clrButton = new JButton("Clear");
+		clrButton.setBounds(284, 200, 117, 29);
+		clrButton.addActionListener(this);
+		contentPane.add(clrButton);
+
 	}
 
+	// Do actions
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == okButton) {
-
+			String fname = fNameField.getText();
+			String lname = lNameField.getText();
+			output.setText("Welcome " + fname + " " + lname);
 		}
 
 		if (e.getSource() == clrButton) {
-
+			fNameField.setText("");
+			lNameField.setText("");
+			output.setText("");
 		}
 	}
 
